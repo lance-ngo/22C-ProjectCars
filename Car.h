b5eff7cd792250c48f8e8371eb46f38635217ebd
jainvidhan <rockstarvidhan@gmail.com>
2017-06-18 19:40:58 -0700

    Update Car.h

--- Car.h
+++ Car.h
@@ -11,6 +11,9 @@ private:
 	std::string make, model, body, trans;
 	int msrp, hp, seats, stock;
 public:
+
+	Car() : make(""), model(""), body(""), trans(""), msrp(0), hp(0), seats(0), stock(0){}
+
 	Car(std::string mke, std::string mdl, int price, int power, int stck, std::string bdy = "", int sts = 0, std::string t = ""){
 		make = mke;
 		model = mdl;
@@ -66,7 +69,28 @@ public:
 		return trans;
 	}
 
-	int operator/(const Car x) const{	//compare two cars, neg value means second car is more expensive
+	bool operator<(const Car& x) const{
+		if (model < x.model)
+			return true;
+		else
+			return false;
+	}
+
+	bool operator<=(const Car& x) const{
+		if (model <= x.model)
+			return true;
+		else
+			return false;
+	}
+
+	bool operator==(const Car& x) const{
+		if (model == x.model)
+			return true;
+		else
+			return false;
+	}
+
+	int operator/(const Car& x) const{	//compare two cars, neg value means second car is more expensive
 		return msrp - x.msrp;
 	}
 
@@ -82,11 +106,10 @@ public:
 
 	friend std::ostream& operator<<(std::ostream& out, Car& x){		//print car details
 		out << std::setw(10) << std::left << x.make << std::setw(16) << std::left << x.model << std::setw(6) << std::right << x.msrp
-			<< std::setw(5) << std::right << x.hp << "  " << std::setw(11) << std::left << x.body << std::setw(5) << x.seats
-			<< std::setw(5) << x.trans << std::setw(5) << x.stock;
+			<< std::setw(5) << std::right << x.hp << "  " << std::setw(11) << std::left << x.body << std::setw(6) << x.seats
+			<< std::setw(8) << x.trans << std::setw(5) << x.stock;
 		return out;
 	}
-		
 };
 
 #endif
